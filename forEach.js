f68b03d79cd6ce7f1d101ed03cdd44ea1a73ec8f
HasnatShahriar <shahriarhriday94@gmail.com>
2024-02-23 22:19:12 +0600

    added the forEach function

---
+++ forEach.js
@@ -0,0 +1,11 @@
+
+// same to map..but,return kore na
+
+const numbers = [2, 3, 5, 6, 7, 8];
+// const result = numbers.forEach(n => console.log(n));
+const result = numbers.forEach(n => {
+  /////
+  /////
+  /////
+});
+console.log(result);
