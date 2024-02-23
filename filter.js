bcf3f6904b207f28579ee91840f22304dc0a3166
HasnatShahriar <shahriarhriday94@gmail.com>
2024-02-23 22:32:59 +0600

    added filter

---
+++ filter.js
@@ -0,0 +1,13 @@
+// const numbers = [65, 78, 55, 66, 69, 74, 73];
+// const evenNumbers = numbers.filter(n => n % 2 ===0);
+// console.log(evenNumbers);
+
+
+// const numbers = [65, 78, 55, 66, 69, 74, 73];
+// const newNumbers = numbers.filter(n => n > 70);
+// console.log(newNumbers);
+
+
+const collegeFriends = ['sajib', 'rizon', 'shawon', 'rojel', 'cornel', 'nahid','Abdullah al masud','rifat hasan'];
+const update = collegeFriends.filter(n => n.length > 7);
+console.log(update);
