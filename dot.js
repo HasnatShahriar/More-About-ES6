b78b22c3e4ac8493aa83ea7b1fb695517e28d0c0
HasnatShahriar <shahriarhriday94@gmail.com>
2024-02-24 11:12:46 +0600

    explain the dot & bracket notation

---
+++ dot.js
@@ -0,0 +1,10 @@
+const person = {
+  name : 'Abdullah',
+  id : 12,
+  'add-ress' : 'kalihati'
+}
+const roll = 'id'
+
+console.log(person.name);
+console.log(person["add-ress"]);
+console.log(person[roll]);
